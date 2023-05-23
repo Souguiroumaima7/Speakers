b8f7089c0b0c4483295e0967a9ec87f0d85a0736
Oumaima Souguir <81293137+Souguiroumaima7@users.noreply.github.com>
2023-05-23 10:46:53 +0100

    push compoments directory and pages directory

---
+++ app/Compoments/InputElementClass.js
@@ -0,0 +1,22 @@
+class InputElementClass extends React.Component {
+    state = {
+      inputText: '',
+    };
+    handleChange = (event) => {
+      this.setState({
+        inputText: event.target.value,
+      });
+    };
+    render() {
+      return (
+        <div>
+          <input onChange={this.handleChange} />
+          <br />
+          {this.state.inputText}
+        </div>
+      );
+    }
+  }
+  
+  export default InputElementClass;
+  
